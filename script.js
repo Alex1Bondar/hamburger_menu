f3b897efa19465874ce37fc688c4ab496815e732
Alex1Bondar <dfkharkov04gmail.com>
2025-04-01 20:50:42 +0200

    created hamburger

---
+++ script.js
@@ -0,0 +1,6 @@
+const hamburger = document.getElementById('hamburger')
+const nav = document.guerySelector('nav ul')
+
+hamburger.addEventListener('click', () => {
+    nav.classList, toggle("active");
+})
